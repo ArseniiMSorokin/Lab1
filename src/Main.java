78b4758df339bad5d30634b8b011bea65460bf1a
ArseniiMSorokin <arseniimsorokin@mail.ru>
2024-10-05 11:42:00 +0300

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,53 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        // Создание объекта sc класса Scanner для ввода данных
+        Scanner sc = new Scanner(System.in);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        // Считывание целочисленных чисел Грузоподъемность, Вес человек A B C
+        short lift_capacity = sc.nextShort();
+        short person_a = sc.nextShort();
+        short person_b = sc.nextShort();
+        short person_c = sc.nextShort();
+
+        // Проверка чтобы хотя бы одного человека лифт мог бы поднять, если не выполняется выводится
+        // 0 - количество людей, которых сможет поднять лифт
+        if (person_a<=lift_capacity || person_b<=lift_capacity || person_c<=lift_capacity) {
+            // Если вес людей А и B меньше или равен грузоподъемности то можно попробовать добавить человека C,
+            // иначе проверить сочетание веса людей А и C
+            if (person_a + person_b <= lift_capacity) {
+                // Если вес людей A B и C меньше или равен грузоподъемности тогда выводим 3 -
+                // то есть лифт может поднять всех людей, иначе лифт может поднять только людей A и C выводим 2
+                if (person_a + person_b + person_c <= lift_capacity) {
+                    System.out.print(3);
+                }
+                else {
+                    System.out.print(2);
+                }
+            }
+            else {
+                // Так как грузоподъемность лифта оказалась меньше веса людей A и B, проверяем сочетание
+                // веса людей A и C, если их сумма меньше или равна грузоподъемности, выводим 2 человека
+                // сможет поднять лифт, иначе проверяем оставшийся случай с весом людей B и C
+                if (person_a + person_c <= lift_capacity) {
+                    System.out.print(2);
+                }
+                else {
+                    // Если вес людей A и C меньше или равен грузоподъемности сразу выводим 2 (так как с третьим
+                    // человеком вес будет точно выше грузоподъемности A+B>X), иначе выводим 1 (так как изначально была
+                    // проверка, что вес хотя бы одного человека меньше или равен грузоподъемности, а какой именно
+                    // человек нам знать не важно)
+                    if (person_b + person_c <= lift_capacity) {
+                        System.out.print(2);
+                    }
+                    else {
+                        System.out.print(1);
+                    }
+                }
+            }
+        }
+        else {
+            System.out.print(0);
         }
     }
 }
